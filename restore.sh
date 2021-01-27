7cc053d2794160b37b9e8d57dd5712ed30a81166
bharatjk <77174243+bharatjk@users.noreply.github.com>
2021-01-26 20:58:57 -0500

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -5,22 +5,29 @@
 BASEDIR=/data
 BACKUPDIR=${BASEDIR}/backup
 Outfile=${BACKUPDIR}/Restore_status.txt
-Restorelog=/tmp/Restore.log
+Restorelog=${BACKUPDIR}/Restore.log
 
 cd $BACKUPDIR
 
 for tarfile in `ls */*.tgz`
     do
       backupfile=`tar tf $tarfile|head -1|cut -f1 -d"/"`
-echo backupfile=$backupfile
       tar xf $tarfile -C /data/openmrs/
       echo "Rstoring $tarfile $backupfile" >>$Restorelog
 
       #restore  task
-      bahmni -i local restore --restore_type=db --options=openmrs --strategy=pitr --restore_point=$backupfile >>$Restorelog
+      bahmni -i local restore --restore_type=db --options=openmrs --strategy=pitr --restore_point=$backupfile 2>&1 >>$Restorelog
 
       #Check Restore status
       mysql -B -u root -pP@ssw0rd openmrs -e "select * from location;"
-      [ $? = 0 ] && echo "$tarfile,$backupfile,Restore_Success,`date +%D,%T`" >>$Outfile || echo "$tarfile,$backupfile,Restore_Failed,`date`" >>$Outfile
+      if [ $? = 0 ] ;then 
+          echo "$tarfile,$backupfile,Restore_Success,`date +%D,%T`" >>$Outfile 
+	  rm -f $Restorelog
+        else
+           echo "$tarfile,$backupfile,Restore_Failed,`date`" >>$Outfile
+	   echo "Restore Filed check restore log @ $Restorelog"
+      fi
+
       [ -d /data/openmrs/$backupfile ] && rm -rf /data/openmrs/$backupfile
+      [ -f /data/openmrs/backup_info.txt ] && rm -rf /data/openmrs/backup_info.txt
 done
